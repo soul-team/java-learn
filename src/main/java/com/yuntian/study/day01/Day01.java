e3b543f09f3bd6beec02877512f84a14af60e354
yeleph <121372841@qq.com>
2019-09-19 23:37:34 +0800

    成绩

---
+++ src/main/java/com/yuntian/study/day01/Day01.java
@@ -0,0 +1,21 @@
+package com.yuntian.study.day01;
+import java.util.Arrays;
+
+public class Day01 {
+    public static  void main(String[] args) {
+        int[] scores ={89,-23,64,91,119,52,73};
+        System.out.print("考试成绩的前三名为：");
+        Day01 test=new Day01();
+        test.getarrays(scores);
+    }
+    public void getarrays(int[] scores){
+        Arrays.sort(scores);
+        int count=1;
+        for(int i= scores.length-1; i>=0&&count<=3; i--) {
+            if (scores[i] < 0 || scores[i] > 100) {
+                continue;
+            }
+            count++;
+            System.out.println(scores[i]);
+        }
+    }
