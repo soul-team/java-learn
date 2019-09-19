4dd624c53c58a8fad431a2ff0bafb3a2a819049a
yuntian <944610721@qq.com>
2019-09-19 23:26:24 +0800

    修改语法错误

--- src/main/java/com/yuntian/study/day01/Day01.java
+++ src/main/java/com/yuntian/study/day01/Day01.java
@@ -1,17 +1,19 @@
 package com.yuntian.study.day01;
+
 import java.util.Arrays;
 
 public class Day01 {
-    public static  void main(String[] args) {
-        int[] scores ={89,-23,64,91,119,52,73};
+    public static void main(String[] args) {
+        int[] scores = {89, -23, 64, 91, 119, 52, 73};
         System.out.print("考试成绩的前三名为：");
-        Day01 test=new Day01();
+        Day01 test = new Day01();
         test.getarrays(scores);
     }
-    public void getarrays(int[] scores){
+
+    public void getarrays(int[] scores) {
         Arrays.sort(scores);
-        int count=1;
-        for(int i= scores.length-1; i>=0&&count<=3; i--) {
+        int count = 1;
+        for (int i = scores.length - 1; i >= 0 && count <= 3; i--) {
             if (scores[i] < 0 || scores[i] > 100) {
                 continue;
             }
@@ -19,3 +21,4 @@ public class Day01 {
             System.out.println(scores[i]);
         }
     }
+}
