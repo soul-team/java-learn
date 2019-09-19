6ada6aee859bf8271c4bc387e9b45a6ccf7bc6dc
yuntian <944610721@qq.com>
2019-09-19 22:05:21 +0800

    团队-java学习项目

---
+++ src/main/java/com/yuntian/study/TestJava.java
@@ -0,0 +1,9 @@
+package com.yuntian.study;
+
+/**
+ * @Auther: yuntian
+ * @Date: 2019/9/18 0018 21:26
+ * @Description:
+ */
+public class TestJava {
+}
---
+++ src/main/java/com/yuntian/study/day01/Day01Demo.java
@@ -0,0 +1,16 @@
+package com.yuntian.study.day01;
+
+/**
+ * @author Administrator
+ * @Auther: yuntian
+ * @Date: 2019/9/18 0018 21:26
+ * @Description: 第一天学习
+ */
+public class Day01Demo {
+
+
+    public static  void main(String[] args){
+        System.out.println("启动。。。");
+        System.out.println("你好：大象妹妹");
+    }
+}
